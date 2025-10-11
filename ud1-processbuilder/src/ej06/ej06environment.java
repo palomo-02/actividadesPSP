bc83c3cb66465aa35c6db911b893840bcec79c92
usuario <palomo.zambrano.jose.manuel@iescamas.es>
2025-10-11 02:05:18 +0200

    añadidos ejercicios hasta el6

--- ud1-processbuilder/src/ej06/ej06environment.java
+++ ud1-processbuilder/src/ej06/ej06environment.java
@@ -2,4 +2,30 @@ package ej06;
 
 public class ej06environment {
 
+	public static void main(String[] args) {
+
+		try {
+
+			ProcessBuilder pb = new ProcessBuilder("printenv","MI_VAR").inheritIO();
+
+			pb.environment().put("MI_VAR", "UD1");
+
+			Process p = pb.start();
+
+			p.waitFor();
+
+			int codigo = p.waitFor();
+			if (codigo == 0) {
+				System.out.println("todo correcto");
+			} else {
+				System.out.println("error en la ejecucion");
+			}
+			
+			
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+
+	}
+
 }
